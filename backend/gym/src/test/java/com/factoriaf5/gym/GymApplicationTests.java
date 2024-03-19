debee2ad2f84c271793bcab4d2821cf2814f1531
Marta <martalarena2002@gmail.com>
2024-03-19 12:15:36 +0100

    creado backend

---
+++ backend/gym/src/test/java/com/factoriaf5/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
