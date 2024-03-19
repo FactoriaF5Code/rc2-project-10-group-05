a0cae9cdae6c045db1bdaa967045eb3d098e35e6
Marta <martalarena2002@gmail.com>
2024-03-19 23:12:02 +0100

    lista e items

---
+++ src/components/MemberList/MemberList.jsx
@@ -0,0 +1,13 @@
+import "./MemberList.css";
+
+export default function MemberList() {
+  return (
+    <>
+      <section className="memberList">
+        <p>Mario</p>
+        <p>De La Roz</p>
+        <p>698698698</p>
+      </section>
+    </>
+  );
+}
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,24 +1,28 @@
+import ListItems from "../../components/ListItems/ListItems";
+import MemberList from "../../components/MemberList/MemberList";
 import "./HomePage.css";
 
 function HomePage() {
-    return (
-        <>
-            <div className="containerHomePage">
-                <section className="containerSearch">
-                    <div className="containerTitleSearch">
-                    <h2>Buscador de Miembros por Nombre</h2>
-                    </div>
-                    <form class="search">
-                        <input type="text" placeholder="Buscar..." />
-                        <button type="submit">Buscar</button>
-                    </form>
-                </section>
-                <section className="containerMemberList">
-                    <h2>Listado de todos los Miembros</h2>
-                </section>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="containerHomePage">
+        <section className="containerSearch">
+          <div className="containerTitleSearch">
+            <h2>Buscador de Miembros por Nombre</h2>
+          </div>
+          <form className="search">
+            <input type="text" placeholder="Buscar..." />
+            <button type="submit">Buscar</button>
+          </form>
+        </section>
+        <section className="containerMemberList">
+          <h2>Listado de todos los Miembros</h2>
+        </section>
+        <ListItems />
+        <MemberList/>
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
