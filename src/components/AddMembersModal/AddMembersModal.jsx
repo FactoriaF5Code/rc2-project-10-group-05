ab8a980bbca4478ba92c5ab1b286766784454a69
Marta <martalarena2002@gmail.com>
2024-03-20 20:52:03 +0100

    fast-refresh error

--- src/components/AddMembersModal/AddMembersModal.jsx
+++ src/components/AddMembersModal/AddMembersModal.jsx
@@ -1,15 +1,25 @@
 /* eslint-disable react/prop-types */
 import "./AddMembersModal.css";
 import cross from "../../assets/cross.png";
+import { useDataContext } from "../DataContext/DataContest";
+import { useState } from "react";
 
-export default function AddMembersModal({onClose}) {
+export default function AddMembersModal({ onClose }) {
+  const { postMember, URL } = useDataContext();
+  const [newMember, setNewMember] = useState({ name: "", lastName: "", contact: "" });
+
+  const handlePost = () => {
+    postMember(newMember, URL);
+    setNewMember({ name: "", lastName: "", contact: "" })
+  };
+
   return (
     <>
       <section className="overlay">
         <section className="contenedorModal">
           <div className="contenedorModal__content">
             <div className="contenedorButton__cerrar">
-              <button className="cerrarModal" onClick={onClose}>
+              <button className="cerrarModal" onClick={() => { onClose(); handlePost(); }}>
                 <img src={cross} alt="x" />
               </button>
             </div>
@@ -27,9 +37,7 @@ export default function AddMembersModal({onClose}) {
                 <input type="text" placeholder="Contacto" />
               </div>
               <div>
-                <button className="finalizarButton">
-                  FINALIZAR
-                </button>
+                <button className="finalizarButton">FINALIZAR</button>
               </div>
             </div>
           </div>
--- src/components/DataContext/DataContest.jsx
+++ src/components/DataContext/DataContest.jsx
@@ -1,16 +1,65 @@
-import { createContext, useContext } from "react";
+
+import { createContext, useContext, useEffect, useState } from "react";
 
 const DataContext = createContext;
 
 export const DataProvider = ({ children }) => {
+  const [member, setmember] = useState([]);
+  const [needsReload, setNeedsReload] = useState(true);
+  const URL = "http://localhost:8080/api/users";
 
+  const fetchData = async (URL) => {
+    try {
+      const response = await fetch(URL);
+      if (response.ok) {
+        const data = await response.json();
+        setmember(data);
+        setNeedsReload(false);
+      } else {
+        console.error("Error al obtener datos");
+      }
+    } catch (error) {
+      console.error("Error de red", error);
+    }
+  };
+
+  useEffect(() => {
+    if (needsReload) {
+      fetchData(URL);
+    }
+  }, [needsReload]);
+
+  const postMember = async (URL, newMember) => {
+    try {
+      const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newMember),
+      };
+
+      const response = await fetch(URL, options);
+
+      if (response.ok) {
+        setmember([]);
+        setNeedsReload(true);
+        alert("Realizado con éxito");
+      } else {
+        alert("Error al realizar la operación");
+      }
+    } catch (error) {
+      console.error("Error de red", error);
+    }
+  };
 
   const value = {
-   
+    postMember,
+    member,
+    URL
   };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
 };
+
 
 export const useDataContext = () => {
   return useContext(DataContext);
