a8b01a8e2645ab3320546f855539d167ae660fd9
Marta <martalarena2002@gmail.com>
2024-03-21 10:52:11 +0100

    post planteado en frontend

--- src/components/DataContext/DataContest.jsx
+++ src/components/DataContext/DataContest.jsx
@@ -1,7 +1,7 @@
 
 import { createContext, useContext, useEffect, useState } from "react";
 
-const DataContext = createContext;
+const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
   const [member, setmember] = useState([]);
